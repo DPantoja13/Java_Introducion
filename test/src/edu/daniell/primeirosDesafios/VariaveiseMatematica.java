6ed7b748a477dda5b947d99d674b28ff54e4e19e
DPantoja13 <dpantoja13360@gmail.com>
2026-07-22 09:23:40 -0300

    second chalenger

---
+++ test/src/edu/daniell/primeirosDesafios/VariaveiseMatematica.java
@@ -0,0 +1,18 @@
+package edu.daniell.primeirosDesafios;
+
+public class VariaveiseMatematica {
+
+    public static void main (String [] args) {
+
+        String produto = "Notebook";
+        float preco = 4599.99f;
+        int quantidade = 2;
+
+        //Calcula o total da compra
+        float total = preco * quantidade;
+
+        System.out.println(quantidade + " " + produto + " custam R$" + total);
+
+    }
+    
+}
