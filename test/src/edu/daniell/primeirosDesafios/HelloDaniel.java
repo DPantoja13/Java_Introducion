310835aa27cce69656b5991486084722a84c16a5
DPantoja13 <dpantoja13360@gmail.com>
2026-07-22 09:15:39 -0300

    fist chalenger

---
+++ test/src/edu/daniell/primeirosDesafios/HelloDaniel.java
@@ -0,0 +1,14 @@
+package edu.daniell.primeirosDesafios;
+
+public class HelloDaniel {
+
+    public static void main(String [] args) {
+
+        String name = "Daniel";
+        int age = 19;
+
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+
+    }
+    
+}
