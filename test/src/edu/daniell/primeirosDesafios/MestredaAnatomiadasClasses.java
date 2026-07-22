8f7dc65122ed0935a448b313e213748d081fc6ab
DPantoja13 <dpantoja13360@gmail.com>
2026-07-22 09:34:39 -0300

    trird chalenger

---
+++ test/src/edu/daniell/primeirosDesafios/MestredaAnatomiadasClasses.java
@@ -0,0 +1,32 @@
+package edu.daniell.primeirosDesafios;
+
+public class MestredaAnatomiadasClasses {
+
+    public static void main(String [] args) {
+
+        /* 
+        Variables coding:
+        name, age, height, gender, isstudent and country 
+        */
+        String name = "Daniel";
+        int age = 19;
+        float height = 1.79f;
+        char gender = 'M';
+        boolean isStudent = true;
+        final String COUNTRY = "Brazil";
+
+        // About me
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Gender: " + gender);
+        System.out.println("Country: " + COUNTRY);
+
+        // Check if the person is a student
+        System.out.println("Is a student: " + isStudent);
+
+        
+
+    }
+    
+}
